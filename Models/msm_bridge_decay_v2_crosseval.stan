bdb35bae246971fdfe4cf5482244ead88af58856
Shoshana Vasserman <shoshievass@Shoshanas-MacBook-Pro.local>
2018-01-12 06:31:26 -0800

    Adding cross vals

---
+++ Models/msm_bridge_decay_v2_crosseval.stan
@@ -0,0 +1,224 @@
+functions{
+  matrix getLowerTriangularMatrix(int T){
+    matrix[T,T] lower_triangular_mat;
+    
+    for(t in 1:T){
+      lower_triangular_mat[t] = append_col(rep_row_vector(1, t), rep_row_vector(0, T-t));
+    }
+    return(lower_triangular_mat);
+  }
+  matrix getDiscountingMatrix(int T,
+                              real delta){
+    matrix[T,T] lower_triangular_mat;
+    matrix[T,T] discounting_mat;
+    vector[T] discounts;
+    
+    lower_triangular_mat = getLowerTriangularMatrix(T);
+    for(t in 1:T){
+      discounts[t] = delta^(t-1);
+    }
+    
+    discounting_mat = diag_matrix(discounts) * lower_triangular_mat;
+    
+    return(discounting_mat);
+  }
+  vector[] forecast_health_state_rng( int N,
+                           int B,
+                           int H,
+                           int M,
+                           int[] deck_health,
+                           int[] superstructure_health,
+                           int[] substructure_health,
+                           int[] T_b,
+                           int[] N_b,
+                           matrix X,
+                           vector spending,
+                           matrix[] beta_deck,
+                           matrix[] beta_superstructure,
+                           matrix[] beta_substructure,
+                           real discount_factor,
+                           int T_f, // number of periods forward
+                           matrix X_f, // projected X_f for T_f periods for each bridge
+                           vector[] spending_f // spending in first period of sim
+                          ){
+    vector[N] out[3];
+    int deck_health_sim[N+(T_f*B)];
+    int superstructure_health_sim[N+(T_f*B)];
+    int substructure_health_sim[N+(T_f*B)];
+    vector[N+(T_f*B)] discounted_spending;
+    matrix[N+(T_f*B), M+1] X_aug_f;
+  
+    int t_b;
+    int n_b;
+    int n;
+    
+    for(b in 1:B){
+      t_b = T_b[b];
+      n_b = N_b[b];
+      
+      deck_health_sim[n_b : n_b + t_b -1] = deck_health[n_b : n_b + t_b -1];
+      superstructure_health_sim[n_b : n_b + t_b -1] = superstructure_health[n_b : n_b + t_b -1];
+      substructure_health_sim[n_b : n_b + t_b -1] = substructure_health[n_b : n_b + t_b -1];
+      
+      discounted_spending[n_b:(n_b + t_b - 1 + T_f)] = getDiscountingMatrix((t_b+T_f), discount_factor) * 
+                      append_row( spending[n_b:(n_b + t_b - 1)], append_row(spending_f[b],rep_vector(0, T_f - 1)));
+      
+      X_aug_f[n_b:(n_b + t_b - 1 + T_f)] = append_col(discounted_spending[n_b:(n_b + t_b - 1 + T_f)], 
+                                            append_row(X[n_b:(n_b + t_b - 1 + T_f)], X_f[(T_f*(b-1) + 1) : T_f*b ])
+                                           );
+      
+      
+      for(t in 1:T_f){
+        n = n_b + t_b - 1 + t;
+        deck_health_sim[n] = categorical_rng(softmax(beta_deck[deck_health_sim[n-1]] * X_aug_f[n]'));
+        superstructure_health_sim[n] = categorical_rng(softmax(beta_superstructure[superstructure_health_sim[n-1]] * X_aug_f[n]'));
+        substructure_health_sim[n] = categorical_rng(softmax(beta_substructure[substructure_health_sim[n-1]] * X_aug_f[n]'));
+  
+      }
+    }
+  
+  out[1] = to_vector(deck_health_sim);
+  out[2] = to_vector(superstructure_health_sim);
+  out[3] = to_vector(substructure_health_sim);
+  return(out);
+  
+  }
+}
+data {
+ int N; // number observations
+ int B; // number unique bridges
+ int M; // number of bridge covariates
+ int H; // max health score possible
+ int deck_health[N];
+ int superstructure_health[N];
+ int substructure_health[N];
+ vector[N] spending;
+ int num_periods_lapsed[N]; // number of years since last observation of this bridge
+ int T_b[B]; // number of periods observed for each bridge
+ int N_b[B]; // number of periods observed for each bridge
+ matrix[N, M] X;
+ //
+ int N_new; // number observations
+ int B_new; // number unique bridges
+ int M_new; // number of bridge covariates
+ int H_new; // max health score possible
+ int deck_health_new[N_new];
+ int superstructure_health_new[N_new];
+ int substructure_health_new[N_new];
+ vector[N_new] spending_new;
+ int num_periods_lapsed_new[N_new]; // number of years since last observation of this bridge
+ int T_b_new[B_new]; // number of periods observed for each bridge
+ int N_b_new[B_new]; // number of periods observed for each bridge
+ matrix[N_new, M_new] X_new;
+ //
+ int<lower=0,upper=1> run_estimation;
+}
+parameters {
+  matrix[H,M+1] beta_deck[H];
+  matrix[H,M+1] beta_superstructure[H];
+  matrix[H,M+1] beta_substructure[H];
+  real discount_scalar;
+}
+transformed parameters{
+  real<lower=0> discount_factor;
+  vector[N] discounted_spending;
+  matrix[N, M+1] X_aug;
+  matrix[N_new, M_new+1] X_aug_new;
+  vector[N_new] discounted_spending_new;
+
+  discount_factor = inv_logit(discount_scalar);
+  
+  for(b in 1:B){
+    discounted_spending[N_b[b]:(N_b[b] + T_b[b] - 1)] = getDiscountingMatrix(T_b[b], discount_factor) * spending[N_b[b]:(N_b[b] + T_b[b] - 1)];
+  }
+  
+  for(b in 1:B_new){
+    discounted_spending_new[N_b_new[b]:(N_b_new[b] + T_b_new[b] - 1)] = getDiscountingMatrix(T_b_new[b], discount_factor) * spending_new[N_b_new[b]:(N_b_new[b] + T_b_new[b] - 1)];
+  }
+  
+  X_aug = append_col(discounted_spending, X);
+  X_aug_new = append_col(discounted_spending_new, X_new);
+
+}
+model {
+  int t_b;
+  int n_b;
+  int n;
+
+  discount_scalar ~ normal(3,1);
+  
+  for(i in 1:H){
+    to_vector(beta_deck[i]) ~ normal(0, 1);
+    to_vector(beta_superstructure[i]) ~ normal(0, 1);
+    to_vector(beta_substructure[i]) ~ normal(0, 1);
+  }
+    
+  for(b in 1:B){
+    t_b = T_b[b];
+    n_b = N_b[b];
+      
+    if(run_estimation==1) {
+
+      for(t in 1:(t_b-1)){
+        n = n_b + t;
+
+        target += log(softmax(beta_deck[deck_health[n-1]] * X_aug[n]')[deck_health[n]]);
+        target += log(softmax(beta_superstructure[superstructure_health[n-1]] * X_aug[n]')[superstructure_health[n]]);
+        target += log(softmax(beta_substructure[substructure_health[n-1]] * X_aug[n]')[substructure_health[n]]);
+
+      }
+    }
+  }
+}
+generated quantities {
+  int deck_health_sim[N];
+  int superstructure_health_sim[N];
+  int substructure_health_sim[N];
+  vector[N] log_posterior_mass;
+  vector[N_new] out_of_sample_lpm;
+  vector[N_new] out_of_sample_lpm_deck;
+  vector[N_new] out_of_sample_lpm_superstructure;
+  vector[N_new] out_of_sample_lpm_substructure;
+
+  int t_b;
+  int n_b;
+  int n;
+  
+  for(b in 1:B){
+    t_b = T_b[b];
+    n_b = N_b[b];
+    
+    log_posterior_mass[n_b] = 0;
+    
+    for(t in 1:(t_b-1)){
+      n = n_b + t;
+      
+      log_posterior_mass[n] = log(softmax(beta_deck[deck_health[n-1]] * X_aug[n]')[deck_health[n]]) + 
+                                log(softmax(beta_superstructure[superstructure_health[n-1]] * X_aug[n]')[superstructure_health[n]]) + 
+                                  log(softmax(beta_substructure[substructure_health[n-1]] * X_aug[n]')[substructure_health[n]]);
+
+    }
+  }
+ 
+  for(b in 1:B_new){
+    t_b = T_b_new[b];
+    n_b = N_b_new[b];
+
+    out_of_sample_lpm[n_b] = 0;
+    out_of_sample_lpm_deck[n_b] = 0;
+    out_of_sample_lpm_superstructure[n_b] = 0;
+    out_of_sample_lpm_substructure[n_b] = 0;
+    
+    for(t in 1:(t_b-1)){
+      n = n_b + t;
+      
+      out_of_sample_lpm_deck[n] = log(softmax(beta_deck[deck_health_new[n-1]] * X_aug_new[n]')[deck_health_new[n]]);
+      out_of_sample_lpm_superstructure[n] = log(softmax(beta_superstructure[superstructure_health_new[n-1]] * X_aug_new[n]')[superstructure_health_new[n]]);
+      out_of_sample_lpm_substructure[n] = log(softmax(beta_substructure[substructure_health_new[n-1]] * X_aug_new[n]')[substructure_health_new[n]]);
+
+      out_of_sample_lpm[n] = out_of_sample_lpm_deck[n] + out_of_sample_lpm_superstructure[n] + out_of_sample_lpm_substructure[n];
+                                  
+    }
+  } 
+  
+}
---
+++ runfiles/estimate_decay_msm_crossval.R
@@ -0,0 +1,280 @@
+library(tidyverse)
+library(skimr)
+
+# Load complete time series
+load("data/bridge_timeseries_merged_by_proj.rdata")
+
+seed(4242)
+
+bridgeIds <- unique(bridge_ts$bridgeID)
+bridge_sample_train <- sample(bridgeIds, size = 400, replace = F)
+bridge_sample_test <- sample(bridgeIds, size = 200, replace = F)
+
+bridge_ts_train <- bridge_ts %>%
+  filter(bridgeID %in% bridge_sample_train)
+
+bridge_ts_train <- bridge_ts_train %>%
+  select(
+    bridgeID,
+    data_year,
+    deck,
+    superstructure,
+    substructure,
+    year_rebuilt,
+    year_built,
+    spending,
+    Structure_Type,
+    Structure_Material,
+    Structure_Length,
+    Type_of_Service
+  ) %>%
+  mutate( # get rid of zero measurements
+    deck = ifelse(deck == 0 | is.na(deck), -999, deck),
+    superstructure = ifelse(superstructure == 0 | is.na(superstructure), -999, superstructure),
+    substructure = ifelse(substructure == 0 | is.na(substructure), -999, substructure),
+  ) %>%
+  group_by(bridgeID) %>%
+  mutate(
+    year_built_latest = cummax(ifelse(is.na(year_rebuilt), -Inf, year_rebuilt))
+    ) %>%
+  ungroup() %>%
+  filter(
+    complete.cases(.)
+  ) %>%
+  select(
+    -year_built,
+    -year_rebuilt
+  ) %>%
+  mutate( # get rid of zero measurements
+    deck = ifelse(deck == -999, NA, deck),
+    superstructure = ifelse(superstructure == -999, NA, superstructure),
+    substructure = ifelse(substructure == -999, NA, substructure),
+  ) %>%
+  arrange(bridgeID, data_year) %>%
+  group_by(
+    bridgeID
+  ) %>%
+  mutate(
+    time_lag = lag(data_year)
+  ) %>%
+  mutate(
+    time_laps = data_year - time_lag,
+    age = pmax(data_year - year_built_latest, 0)
+  ) %>%
+  ungroup() %>%
+  filter(
+    complete.cases(.)
+  ) 
+
+T_b_df <- bridge_ts_train %>%
+  mutate(rn = row_number()) %>%
+  group_by(bridgeID) %>%
+  summarize(
+    T_b = n(),
+    n_b = first(rn)
+  )
+
+B <- length(unique(bridge_ts_train$bridgeID))
+
+bridge_ts_train <- bridge_ts_train %>%
+  left_join(T_b_df)
+
+X_bridge_train <- model.matrix(~ scale(age), data = bridge_ts_train)
+
+
+## get test data
+bridge_ts_test <- bridge_ts %>%
+  filter(bridgeID %in% bridge_sample_test)
+
+bridge_ts_test <- bridge_ts_test %>%
+  select(
+    bridgeID,
+    data_year,
+    deck,
+    superstructure,
+    substructure,
+    year_rebuilt,
+    year_built,
+    spending,
+    Structure_Type,
+    Structure_Material,
+    Structure_Length,
+    Type_of_Service
+  ) %>%
+  mutate( # get rid of zero measurements
+    deck = ifelse(deck == 0 | is.na(deck), -999, deck),
+    superstructure = ifelse(superstructure == 0 | is.na(superstructure), -999, superstructure),
+    substructure = ifelse(substructure == 0 | is.na(substructure), -999, substructure),
+  ) %>%
+  group_by(bridgeID) %>%
+  mutate(
+    year_built_latest = cummax(ifelse(is.na(year_rebuilt), -Inf, year_rebuilt))
+  ) %>%
+  ungroup() %>%
+  filter(
+    complete.cases(.)
+  ) %>%
+  select(
+    -year_built,
+    -year_rebuilt
+  ) %>%
+  mutate( # get rid of zero measurements
+    deck = ifelse(deck == -999, NA, deck),
+    superstructure = ifelse(superstructure == -999, NA, superstructure),
+    substructure = ifelse(substructure == -999, NA, substructure),
+  ) %>%
+  arrange(bridgeID, data_year) %>%
+  group_by(
+    bridgeID
+  ) %>%
+  mutate(
+    time_lag = lag(data_year)
+  ) %>%
+  mutate(
+    time_laps = data_year - time_lag,
+    age = pmax(data_year - year_built_latest, 0)
+  ) %>%
+  ungroup() %>%
+  filter(
+    complete.cases(.)
+  ) 
+
+T_b_df_test <- bridge_ts_test %>%
+  mutate(rn = row_number()) %>%
+  group_by(bridgeID) %>%
+  summarize(
+    T_b = n(),
+    n_b = first(rn)
+  )
+
+B_test <- length(unique(bridge_ts_test$bridgeID))
+
+bridge_ts_test <- bridge_ts_test %>%
+  left_join(T_b_df_test)
+
+X_bridge_test <- model.matrix(~ scale(age), data = bridge_ts_test)
+
+library(rstan)
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+dgp_model <- stan_model("Models/msm_bridge_decay_v2_crosseval.stan")
+
+w <- 1e6 # for scaling spending
+
+real_data_list <- list(
+  N = nrow(bridge_ts_train),
+  B = length(unique(bridge_ts_train$bridgeID)),
+  M = ncol(X_bridge_train),
+  H = max(unique(bridge_ts_train$deck)),
+  deck_health = bridge_ts_train$deck,
+  superstructure_health = bridge_ts_train$superstructure,
+  substructure_health = bridge_ts_train$substructure,
+  spending = (bridge_ts_train$spending / w),
+  num_periods_lapsed = bridge_ts_train$time_laps,
+  T_b = T_b_df$T_b,
+  N_b = T_b_df$n_b,
+  X = X_bridge_train,
+  # 
+  N_new = nrow(bridge_ts_test),
+  B_new = length(unique(bridge_ts_test$bridgeID)),
+  M_new = ncol(X_bridge_test),
+  H_new = max(unique(bridge_ts_test$deck)),
+  deck_health_new = bridge_ts_test$deck,
+  superstructure_health_new = bridge_ts_test$superstructure,
+  substructure_health_new = bridge_ts_test$substructure,
+  spending_new = (bridge_ts_test$spending / w),
+  num_periods_lapsed_new = bridge_ts_test$time_laps,
+  T_b_new = T_b_df_test$T_b,
+  N_b_new = T_b_df_test$n_b,
+  X_new = X_bridge_test,
+  # 
+  run_estimation = 1
+)
+
+system.time(model_fit_opt <- optimizing(dgp_model, data = real_data_list, verbose = T))
+
+get_opt_est <- function(x, par) {
+  x$par[grepl(par, names(x$par))]
+}
+
+# mle_beta_deck
+# mle_beta_deck_vec <- as.matrix(mle_beta_deck)
+# 
+# mle_beta_deck <- get_opt_est(model_fit_opt, "\\bbeta_deck\\b")
+# mle_beta_superstructure <- get_opt_est(model_fit_opt, "\\bbeta_superstructure\\b")
+# mle_beta_substructure <- get_opt_est(model_fit_opt, "\\bbeta_substructure\\b")
+mle_discount_scalar <- get_opt_est(model_fit_opt, "\\bdiscount_scalar\\b")
+mle_X_aug <- get_opt_est(model_fit_opt, "\\bX_aug\\b")
+arm::invlogit(mle_discount_scalar)
+
+estimated_model <- sampling(dgp_model, data = real_data_list, iter = 500, chains = 4, cores = 4)
+
+print(estimated_model, pars="discount_scalar")
+print(estimated_model)
+
+log_posterior_mass <- get_posterior_mean(estimated_model, pars = "log_posterior_mass")[, 5]
+sum(log_posterior_mass)
+
+mean(exp(log_posterior_mass/3))
+# plot(exp(log_posterior_mass/3))
+
+data.frame(mean_prob = exp(log_posterior_mass/3),
+           deck_health = factor(bridge_ts_train$deck)) %>%
+  ggplot(aes(x = mean_prob, y = deck_health, fill = deck_health)) + geom_density_ridges(alpha = 0.3)
+
+oos_log_posterior_mass <- get_posterior_mean(estimated_model, pars = "out_of_sample_lpm")[, 5]
+sum(oos_log_posterior_mass)
+mean(exp(oos_log_posterior_mass/3))
+
+data.frame(oos_mean_prob = exp(oos_log_posterior_mass/3),
+           deck_health = factor(bridge_ts_test$deck)) %>%
+  ggplot(aes(x = oos_mean_prob, y = deck_health, fill = deck_health)) + geom_density_ridges(alpha = 0.3)
+
+
+oos_log_posterior_mass_deck <- get_posterior_mean(estimated_model, pars = "out_of_sample_lpm_deck")[, 5]
+oos_log_posterior_mass_superstructure <- get_posterior_mean(estimated_model, pars = "out_of_sample_lpm_superstructure")[, 5]
+oos_log_posterior_mass_substructure <- get_posterior_mean(estimated_model, pars = "out_of_sample_lpm_substructure")[, 5]
+
+
+oos_lpm_df <- 
+  data.frame(oos_mean_prob = exp(oos_log_posterior_mass/3),
+                         oos_prob_deck = exp(oos_log_posterior_mass_deck),
+                         oos_prob_superstructure = exp(oos_log_posterior_mass_superstructure),
+                         oos_prob_substructure = exp(oos_log_posterior_mass_substructure),
+                         deck_health = (bridge_ts_test$deck),
+                         superstructure_health = (bridge_ts_test$superstructure),
+                         substructure_health = bridge_ts_test$substructure
+                         ) %>%
+  mutate(
+    min_health = pmin(deck_health,superstructure_health, substructure_health),
+    min_health_state = factor(min_health)
+  ) %>%
+  select(
+    -deck_health,
+    -superstructure_health,
+    -substructure_health,
+    -min_health
+  ) %>%
+  rename(
+    deck = oos_prob_deck,
+    superstructure = oos_prob_superstructure,
+    substructure = oos_prob_substructure
+  ) %>%
+  gather(
+    key = "structure",
+    value = "model_probability",
+    - min_health_state,
+    -oos_mean_prob
+  )
+
+oos_lpm_df %>%
+  ggplot(aes(x=model_probability, y=min_health_state, fill = structure)) + 
+  geom_density_ridges(position="dodge", alpha=0.3, from = 0, to =1 ) + theme_ridges() +
+  labs(
+    title = "Model Probability of Observed State in Test Data",
+    x = "Model Probability of Realized Outcome State",
+    y = "Minimum Outcome State"
+  )
+
+ggsave("graphs/cross_val_demo.png", height = 8, width = 12)
